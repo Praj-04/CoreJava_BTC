489c7ec891901ae48b83085bcf9bbb039ef281ed
Prajna Yogesh <prajna.yogish4@gmail.com>
2023-12-26 23:10:30 -0500

    Assertion

---
+++ Assertion/src/com/demo/assertions/AssertDemo.java
@@ -0,0 +1,13 @@
+package com.demo.assertions;
+
+public class AssertDemo {
+
+	public static void main(String[] args) {
+int withdrawal =200;
+int currentBalance = 160;
+
+assert(withdrawal<currentBalance):"Withdrawal amount is more than current balance";
+		
+	}
+
+}
