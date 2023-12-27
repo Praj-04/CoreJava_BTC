c06145fc28c18d1a56da90b817f2fd237a48a84d
Prajna Yogesh <prajna.yogish4@gmail.com>
2023-12-26 23:17:51 -0500

    Innerclasses

---
+++ InnerClasses/src/com/demo/nonstaticinnerclass/Outer.java
@@ -0,0 +1,26 @@
+package com.demo.nonstaticinnerclass;
+
+public class Outer {
+
+	void f1() {
+		System.out.println("Outer class- non static method");
+	}
+
+	class Inner {
+
+		void f2() {
+			System.out.println("Inner class- non static method");
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		Outer outer = new Outer();
+		outer.f1();
+
+		Outer.Inner inner = outer.new Inner();
+		inner.f2();
+	}
+
+}
